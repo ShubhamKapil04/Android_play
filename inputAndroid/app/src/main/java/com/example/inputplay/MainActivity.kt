0f35a855349616ec39e196358876b84c0239b4d3
Shubham Kapil <skaplesh720@gmail.com>
2023-01-30 15:45:09 +0530

    -m Input lines

--- inputAndroid/app/src/main/java/com/example/inputplay/MainActivity.kt
+++ inputAndroid/app/src/main/java/com/example/inputplay/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.inputplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val num = findViewById<TextView>(R.id.num)
+        val button = findViewById<Button>(R.id.btn)
+
+
+    }
+
+    fun btn(view: View) {
+        val num = findViewById<TextView>(R.id.num)
+        val button = findViewById<Button>(R.id.btn)
+
+        button.setOnClickListener {
+            num.setText("1")
+        }
     }
 }
