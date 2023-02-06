8b5a9e8299cea597d5d5e165dfecee2775bacd3a
Shubham Kapil <skaplesh720@gmail.com>
2023-02-06 21:17:41 +0530

    Taking user input

--- inputAndroid/app/src/main/java/com/example/inputplay/MainActivity.kt
+++ inputAndroid/app/src/main/java/com/example/inputplay/MainActivity.kt
@@ -2,27 +2,22 @@ package com.example.inputplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var counterText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val num = findViewById<TextView>(R.id.num)
-        val button = findViewById<Button>(R.id.btn)
-
+        counterText = findViewById(R.id.num)
 
     }
 
     fun btn(view: View) {
-        val num = findViewById<TextView>(R.id.num)
-        val button = findViewById<Button>(R.id.btn)
-
-        button.setOnClickListener {
-            num.setText("1")
-        }
+        counterText.text = "${counterText.text.toString().toInt() + 1 * 2}"
     }
 }
