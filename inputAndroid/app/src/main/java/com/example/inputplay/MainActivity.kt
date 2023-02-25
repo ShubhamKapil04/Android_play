8230d3197d57ed2f0640fdc102d8c2b5f5c8be72
Shubham Kapil <skaplesh720@gmail.com>
2023-02-25 22:40:02 +0530

    Added Order Summery

--- inputAndroid/app/src/main/java/com/example/inputplay/MainActivity.kt
+++ inputAndroid/app/src/main/java/com/example/inputplay/MainActivity.kt
@@ -1,5 +1,8 @@
+@file:Suppress("SameParameterValue")
+
 package com.example.inputplay
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,15 +13,21 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var counterText: TextView
     lateinit var numText: TextView
+    var numberofCup : Int = 0
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         counterText = findViewById(R.id.num)
         numText = findViewById(R.id.num)
         val button = findViewById<Button>(R.id.btn)
         val inc = findViewById<Button>(R.id.increment_btn)
         val dec = findViewById<Button>(R.id.decrement_btn)
-        var numberofCup : Int = 0
+
+
+
         inc.setOnClickListener() {
             numberofCup++
             numText.text = "${numberofCup}"
@@ -31,23 +40,29 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener() {
             counterText.text = "${numberofCup}"
             displayPrice(numberofCup)
-//            text = "${counterText.text.toString().toInt()}"
         }
     }
 
      private fun displayPrice(numberofCup: Int) {
-         //This will store the view for price
-         val priceText = findViewById<TextView>(R.id.price_text_view)
+
          val price_of_Cup = 5
-//         priceText = numberofCup + price_of_Cup
-         priceText.setText("Total : $${numberofCup * price_of_Cup}")
-         displayMessage()
+         val total_price = calculatePrice(numberofCup, price_of_Cup)
+         displayMessage(total_price)
      }
 
-    private fun displayMessage(){
-        val message: String = "ThankYou"
-        var messageD = findViewById<TextView>(R.id.message)
-        messageD.setText(message)
+    private fun displayMessage(total_price: Int){
+        val priceText = findViewById<TextView>(R.id.order_summary_text_view)
+//        priceText.text = total_price.toString()
+        val message: String =  ("Name: Shubham Kapil \n" +
+                                "Quantity: $numberofCup \n" +
+                                "Total: $total_price$ \n" +
+                                "Thank You!")
+
+        priceText.text = message
+    }
+
+    private fun calculatePrice(numberofCup: Int, price_of_cup: Int): Int{
+        return numberofCup * price_of_cup
     }
 
 
